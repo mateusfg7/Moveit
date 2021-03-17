0cf651c3a39b39e5213d689254064d3583428e51
Mateus Felipe Gonçalves <mateusfelipefg77@gmail.com>
2021-03-17 15:44:49 -0300

    use countdown context on Countdown component

--- src/components/Countdown.tsx
+++ src/components/Countdown.tsx
@@ -1,40 +1,22 @@
-import { useState, useEffect } from 'react';
-import { IoCheckmarkCircleSharp } from 'react-icons/io5'
+import { useContext } from 'react';
+import { IoCheckmarkCircleSharp } from 'react-icons/io5';
+
+import { CountdownContext } from '../contexts/CountdownContext';
 
 import styles from '../styles/components/Countdown.module.css';
 
-let countdownTimeout: NodeJS.Timeout
-
 export function Countdown() {
-  const [time, setTime] = useState(0.1 * 60);
-  const [isActive, setIsActive] = useState(false);
-  const [hasFinished, setHasFinished] = useState(false);
-
-  const minutes = Math.floor(time / 60);
-  const seconds = time % 60
+  const {
+    minutes,
+    seconds,
+    hasFinished,
+    isActive,
+    startCountdown,
+    resetCountdown,
+  } = useContext(CountdownContext);
 
   const [minuteLeft, minuteRight] = String(minutes).padStart(2, '0').split('');
   const [secondLeft, secondRight] = String(seconds).padStart(2, '0').split('');
-
-  function startCountdown() {
-    setIsActive(true);
-  }
-
-  function resetCountdown() {
-    clearTimeout(countdownTimeout);
-    setIsActive(false);
-    setTime(0.1 * 60);
-  }
-  useEffect(() => {
-    if (isActive && time > 0) {
-      countdownTimeout = setTimeout(() => {
-        setTime(time - 1);
-      }, 1000);
-    } else if (isActive && time === 0) {
-      setHasFinished(true);
-      setIsActive(false);
-    }
-  }, [isActive, time]);
 
   return (
     <div>
@@ -50,36 +32,32 @@ export function Countdown() {
         </div>
       </div>
 
-      { hasFinished ? (
-        <button
-          disabled
-          className={styles.countdownButton}
-        >
-          <span>Ciclo encerrado</span>  <IoCheckmarkCircleSharp className={styles.checkIcon} />
+      {hasFinished ? (
+        <button disabled className={styles.countdownButton}>
+          <span>Ciclo encerrado</span>{' '}
+          <IoCheckmarkCircleSharp className={styles.checkIcon} />
         </button>
       ) : (
-          <>
-            {isActive ? (
-              <button
-                type='button'
-                className={`${styles.countdownButton} ${styles.countdownButtonActive}`}
-                onClick={resetCountdown}
-              >
-                Abandonar ciclo
-              </button>
-            ) : (
-                <button
-                  type='button'
-                  className={styles.countdownButton}
-                  onClick={startCountdown}
-                >
-                  Iniciar um ciclo
-                </button>
-              )}
-          </>
-        )}
-
-
+        <>
+          {isActive ? (
+            <button
+              type='button'
+              className={`${styles.countdownButton} ${styles.countdownButtonActive}`}
+              onClick={resetCountdown}
+            >
+              Abandonar ciclo
+            </button>
+          ) : (
+            <button
+              type='button'
+              className={styles.countdownButton}
+              onClick={startCountdown}
+            >
+              Iniciar um ciclo
+            </button>
+          )}
+        </>
+      )}
     </div>
   );
 }
